d5df7012d654ccbc4e69ae81c2c425da36750a5d
Adam Rackis <adamrackis@hotmail.com>
2024-12-07 18:38:12 -0600

    cleanup

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,23 +1,4 @@
 import { createFileRoute, Link } from "@tanstack/react-router";
-import { createServerFn } from "@tanstack/start";
-
-export const serverFn1 = createServerFn({ method: "GET" })
-  .validator((x: string) => {
-    return 12;
-  })
-  .handler((ctx) => {
-    const x = ctx.data; // x is any
-    return { time: "" };
-  });
-
-export const serverFn2 = createServerFn({ method: "GET" })
-  .validator((x: string) => {
-    return 12 as const;
-  })
-  .handler((ctx) => {
-    const x = ctx.data; // x is any
-    return { time: "" };
-  });
 
 export const Route = createFileRoute("/")({
   component: Home,
