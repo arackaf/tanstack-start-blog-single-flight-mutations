ef2d7ddd0750c8402b7ad62a90a4a4c47816b4ce
Adam Rackis <adamrackis@hotmail.com>
2024-12-07 19:38:04 -0600

    Fix backend

--- app.config.js
+++ app.config.js
@@ -1,3 +1,9 @@
 import { defineConfig } from "@tanstack/start/config";
 
-export default defineConfig({});
+export default defineConfig({
+  server: {
+    routeRules: {
+      "/api/**": { proxy: "http://localhost:3001/api/**" },
+    },
+  },
+});
