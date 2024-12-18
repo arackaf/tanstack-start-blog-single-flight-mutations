fbe22df2eda526edd36689ded83a90b376e5f396
Adam Rackis <adamrackis@hotmail.com>
2024-12-18 09:43:30 -0600

    single-flight tweaks

--- app/routes/app/epics/$epicId/edit.tsx
+++ app/routes/app/epics/$epicId/edit.tsx
@@ -7,43 +7,60 @@ import { useEffect, useRef, useState } from "react";
 import { postToApi } from "../../../../../backend/fetchUtils";
 import { createServerFn } from "@tanstack/start";
 import { epicsQueryOptions } from "../../../../queries/epicsQuery";
+import { queryClient } from "../../../../router";
 
-const reactQueryMiddleware = createMiddleware().server(async ({ next, context }) => {
-  console.log("Middleware server before", { context });
-  const serverFnResult = await next({ sendContext: { xyz: 999 } });
+const reactQueryMiddleware = createMiddleware()
+  .client(async ({ next, context, sendContext }) => {
+    console.log("Client before");
 
-  console.log("Middleware server after", { result: serverFnResult });
+    try {
+      const res = await next();
+      console.log("in client", "result", { res });
+      return res;
+    } catch (er) {
+      console.log({ er });
+      throw er;
+    } finally {
+      console.log("Client after", { context, sendContext });
+    }
+  })
+  .server(async ({ next, context }) => {
+    console.log("Middleware server before", { context });
 
-  // @ts-ignore
-  serverFnResult.result.message = "ayyyyyyyyyyy";
+    const serverFnResult = await next({ sendContext: { xyz: 999 } });
 
-  const queryOptions = epicsQueryOptions(0, 1);
-  const data = await queryOptions.queryFn();
-  console.log({ data });
+    console.log("Middleware server after", { result: serverFnResult });
 
-  // @ts-ignore
-  serverFnResult.context.qwert = "Hi";
-  serverFnResult.result.newData = data;
+    const epicsListOptions = epicsQueryOptions(0, 1);
+    const epicOptions = epicQueryOptions(0, "1");
 
-  return serverFnResult;
-});
+    const listData = await epicsListOptions.queryFn();
+    const epicData = await epicOptions.queryFn();
+
+    console.log({ listData, epicData });
+
+    serverFnResult.clientAfterContext = { xyz: 12, www: "Hello" };
+    serverFnResult.result.query = {
+      listData,
+      epicData,
+    };
+    return serverFnResult;
+  });
 
 export const saveEpic = createServerFn({ method: "POST" })
   .middleware([reactQueryMiddleware])
   .validator((updates: { id: string; newName: string }) => updates)
   .handler(async ({ data, context }) => {
     console.log("Running server function");
+
     await postToApi("api/epic/update", {
       id: data.id,
       name: data.newName,
     });
     console.log("Server function finished", { context });
 
-    return {
-      a: 12,
-    };
-
-    //throw redirect({ to: "/app/epics", search: { page: 1 } });
+    return {};
+    throw redirect({ to: "/app/epics", search: { page: 1 } });
   });
 
 export const Route = createFileRoute("/app/epics/$epicId/edit")({
@@ -82,12 +99,27 @@ function EditEpic() {
 
     console.log({ result });
 
-    const queryOptions = epicsQueryOptions(0, 1);
-    result.newData;
+    const listOptions = epicsQueryOptions(0, 1);
+    const epicOptions = epicQueryOptions(0, "1");
+    //result.newData;
 
-    queryClient.invalidateQueries({ queryKey: ["epics", "list"] });
-    queryClient.invalidateQueries({ queryKey: ["epic", epicId] });
-    queryClient.setQueryData(queryOptions.queryKey, result.newData, { updatedAt: Date.now() });
+    const a = queryClient.getQueriesData({ queryKey: ["epics"] });
+    const b = queryClient.getQueriesData({ queryKey: ["epic"] });
+
+    console.log({ a, b });
+
+    queryClient.invalidateQueries({ queryKey: ["epics"], refetchType: "none" });
+    queryClient.invalidateQueries({ queryKey: ["epic"], refetchType: "none" });
+
+    queryClient.setQueryData(["epics", "list", 1], result.query.listData, { updatedAt: Date.now() });
+    queryClient.setQueryData(["epic", "1"], result.query.epicData, { updatedAt: Date.now() });
+
+    queryClient.refetchQueries({ queryKey: ["epics"], type: "active", stale: true });
+    queryClient.refetchQueries({ queryKey: ["epic"], type: "active", stale: true });
+
+    console.log({ key: epicOptions.queryKey, data: result.query.epicData });
+
+    setSaving(false);
   };
 
   useEffect(() => {
@@ -103,7 +135,8 @@ function EditEpic() {
       </div>
       <div>
         <div className="flex flex-col gap-2">
-          <span>Edit epic {epicId}</span>
+          <span>Edit epic {epic.id}</span>
+          <span>{epic.name}</span>
           <input className="self-start border p-2 w-64" ref={newName} defaultValue={epic.name} />
           <div className="flex gap-2"></div>
           <button className="self-start p-2 border" onClick={save}>
